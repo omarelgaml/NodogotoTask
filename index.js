f33cc86b777f1d557b7ac268227a799ad4270119
omarelgaml <omarelgaml97@gmail.com>
2020-03-30 01:52:57 +0200

    deploy

--- index.js
+++ index.js
@@ -19,6 +19,22 @@ app.use (passport.initialize ());
 app.use (passport.session ());
 require ('./routes/authRoutes') (app);
 
+
+if (process.env.NODE_ENV === 'production') {
+  //express will serve up the index.html file
+  //ilike our main.js file, or main.css file
+
+  app.use (express.static ('client/build'));
+
+  //express will serve up the index.html file
+  // if it does not recognize the route
+  app.get ('*', (req, res) => {
+    res.sendFile (path.resolve (__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+
+
 const PORT = process.env.PORT || 5000;
 app.listen (PORT);
 
