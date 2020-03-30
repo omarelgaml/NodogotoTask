76ba26fc1c8656c92f26eea3e5e22a4b2b9c5437
omarelgaml <omarelgaml97@gmail.com>
2020-03-30 02:17:51 +0200

    deploy

--- index.js
+++ index.js
@@ -28,8 +28,9 @@ if (process.env.NODE_ENV === 'production') {
 
   //express will serve up the index.html file
   // if it does not recognize the route
+  const path = require('path');
   app.get ('*', (req, res) => {
-    res.sendFile (path.resolve (__dirname, 'client/build', 'index.html'));
+    res.sendFile (path.resolve (__dirname, 'client', 'build', 'index.html'));
   });
 }
 
